10c76c99f1a737c544078e13004959444fcfffd7
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-07-11 17:52:04 +0900

    Update BossHunter(상속과 클래스).cpp

--- cpp_peoblem/BossHunter(상속과 클래스).cpp
+++ cpp_peoblem/BossHunter(상속과 클래스).cpp
@@ -12,50 +12,65 @@ private:
 public:
 	int Attack();//공격
 	int Hitted(int damage);//피격
-	//void GetEXP();//경험치 획득
+	int GetEXP() { return Mob_EXP; }//경험치 획득
+	bool IsMobDeath();//사망
 };
-int Monster::Attack() {
+int Monster::Attack() {//몬스터 공격
 	int maxDamage = Mob_attack;
 	int minDamage = maxDamage * 8 / 10;
 	return minDamage + (rand() % (maxDamage - minDamage + 1));
 }
-int Monster::Hitted(int damage) {
+int Monster::Hitted(int damage) {//몬스터 피격
 	Mob_HP = max(0, Mob_HP - damage);
 	return Mob_HP;
 }
+bool Monster::IsMobDeath() {
+	if (Mob_HP <= 0) return true;
+	return false;
+}
+
 //유저
 class User {
 protected://자식이 써야함
-	const int user_max_hp = 3000;
-	const int user_max_mp = 1200;
+	const int user_max_hp = 3000; 
+	const int user_max_mp = 3000;
 	const int user_max_attack = 900;
 	int user_hp = user_max_hp;
 	int user_mp = user_max_mp;
 	int user_min_attack = user_max_attack * 9 / 10;
 	int user_attack;
 public:
-	static int max(int x, int y);
-	bool Death();
-	void Attacked(int damage);
+	static int max(int x, int y);//max함수 정의
+	bool IsDeath();//사망 처리
+	void Attacked(int damage);//피격
+	void Heal();//회복
 };
 
 //user 사망
-bool User::Death() {
+bool User::IsDeath() {
 	if (user_hp <= 0) return true;
 	return false;
 }
+//max함수 정의
 int User::max(int x, int y) {
 	return x > y ? x : y;
 }
+//피격
 void User::Attacked(int damage) {
 	user_hp = max(0, user_hp - damage);
 	cout <<"HP : "<< user_hp<<" / "<<user_max_hp<<"\n";
+}
+//회복
+void User::Heal() {
+	user_hp = user_max_hp;
+	user_mp = user_max_mp;
+	cout << "HP : " << user_hp << " / " << user_max_hp << "\n";
 }
 
 //임페일
 class Impale : public User {
 private:
-	int Impale_percentage = 3;//퍼뎀
+	int Impale_percentage = 2;//퍼뎀
 	int cost_mp = 30;//소모mp
 	int Skill_count = 6;//공격횟수
 public:
@@ -67,9 +82,9 @@ void Impale::consum_MP(int cost_mp) {
 	user_mp = max(0,user_mp-cost_mp);
 }
 int Impale::Attack() {
-	int sumDamage = 0;
+	int sumDamage = 0;//총합 데미지
 	for (int i = 0; i < Skill_count; i++) {
-		user_attack = user_min_attack + rand() % (user_max_attack - user_min_attack + 1);
+		user_attack = user_min_attack + rand() % (user_max_attack - user_min_attack + 1);//한줄 데미지
 		int presentDamage = Impale_percentage * user_attack;
 		cout << presentDamage << "\n";
 		sumDamage += presentDamage;
@@ -87,8 +102,13 @@ int main(){
 	Monster boss;
 	User user;
 	Impale impale;
-	cout <<"Boss : " << boss.Hitted(impale.Attack()) << "\n";
-	cout << boss.Attack() << "\n";
-	user.Attacked(boss.Attack());
+
+	while (!boss.IsMobDeath()) {
+		cout << "Boss : " << boss.Hitted(impale.Attack()) << "\n";
+		cout << boss.Attack() << "\n";
+		user.Attacked(boss.Attack());
+		user.Heal();
+		cout << "\n";
+	}
     return 0;
 }
