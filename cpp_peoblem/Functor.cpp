b63d027e1f3c8ad72c5a82cfb15c2363d91a543c
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-10-07 18:47:57 +0900

    Create Functor.cpp

---
+++ cpp_peoblem/Functor.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+#pragma warning(disable:4996) // C4996 에러를 무시
+
+class Point {
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y = 0) : xpos(x), ypos(y) {}
+	Point operator+(const Point& pos) const {
+		return Point(xpos + pos.xpos, ypos + pos.ypos);
+	}
+	friend ostream& operator<<(ostream& os, const Point& pos);
+};
+ostream& operator<<(ostream& os, const Point& pos) {
+	os << "[" << pos.xpos << " " << pos.ypos << "]\n";
+	return os;
+}
+class Adder {
+public:
+	int operator()(const int& n1,const int& n2) {
+		return n1 + n2;
+	}
+	Point operator()(const Point& pos1, const Point& pos2) {
+		return pos1 + pos2;
+	}
+};
+int main() {
+	//빠른 입력
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	Adder adder;
+	cout << adder(64, 36)<<"\n";
+	cout << adder(Point(3, 4), Point(7, 9));
+	return 0;
+}
