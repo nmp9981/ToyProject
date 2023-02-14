4beebe4504d380ca233622571a92ff7c6effa224
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-02-14 11:00:08 +0900

    Create 11.cpp

---
+++ cpp_peoblem/명품연습문제/chap3/11.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class House{
+    int numOfRooms;
+    int size;
+public:
+    House(int n,int s);
+    ~House();
+};
+//생성자
+House::House(int n,int s){
+    numOfRooms = n;
+    size = s;
+    cout<<numOfRooms<<" "<<size<<"\n";
+}
+//소멸자
+House::~House(){
+    cout<<numOfRooms<<" "<<size<<"소멸\n";
+}
+void f(){
+    House a(2,20);
+}
+
+House b(3,30),c(4,40);
+int main()
+{
+    f();
+    House d(5,50);
+    return 0;
+}
